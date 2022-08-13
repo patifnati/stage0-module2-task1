211250fc6a29619f507b0eaf53a9b378d68a2c0a
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-13 21:24:27 +0200

    I add one code line and a comment.

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -3,6 +3,6 @@ package com.mjc.stage0;
 public class FirstApplication {
     public static void main(String[] args) {
 System.out.println("HelloWorld!");
-        // Write your code here!
+        // There is my code.
     }
 }
